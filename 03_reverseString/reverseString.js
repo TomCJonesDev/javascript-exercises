8193c437d9ec4eea9edabc4bf6a969b4b6235a27
TomCJonesDev <196759137+TomCJonesDev@users.noreply.github.com>
2025-02-12 14:30:23 +0000

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split("");
+    let finalArray = [];
+    let finalString = "";
+    for (let char of array) {
+        finalArray.unshift(char);
+    }
+    finalString = finalArray.join("");
+    return finalString;
 };
 
 // Do not edit below this line
