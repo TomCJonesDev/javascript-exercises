0343673fae46f3a3981cef1c6d434cc77fb03507
TomCJonesDev <196759137+TomCJonesDev@users.noreply.github.com>
2025-02-12 14:04:08 +0000

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, multiplier) {
+    if (multiplier >= 0) {
+        let finalString = "";
+        for (i = 0; i < multiplier; i++) {
+            finalString += string;
+        }
+        return finalString; 
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
