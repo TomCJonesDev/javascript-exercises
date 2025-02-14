953be4a6c84f2e53811538d5f5300563fc0cf08f
TomCJonesDev <196759137+TomCJonesDev@users.noreply.github.com>
2025-02-14 22:23:30 +0000

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let sorted = array.sort((a, b) => {
+        if (!a.yearOfDeath) {
+            a.yearOfDeath = new Date().getFullYear();
+        }
+        if (!b.yearOfDeath) {
+            b.yearOfDeath = new Date().getFullYear();
+        }
+        return (a.yearOfDeath - a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth) ? 1 : -1
+    });
+    return sorted[0];
 };
 
 // Do not edit below this line
