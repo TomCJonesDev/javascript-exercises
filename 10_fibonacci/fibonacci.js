a265a76838c857040c9557ee32b3bffc291cccdc
TomCJonesDev <196759137+TomCJonesDev@users.noreply.github.com>
2025-02-14 21:58:22 +0000

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let array = [1, 1];
+    for (let i = 2; i < n; i++) {
+        array[i] = array[i-2] + array[i-1]
+    }
+    if (n > 0) {
+        return array[n -1];
+    } else if (n < 0) {
+        return "OOPS";
+    } else {
+        return 0;
+    }
 };
 
 // Do not edit below this line
