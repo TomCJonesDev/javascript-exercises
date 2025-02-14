522a2527da83f4923ed5438f8b426a70e63d7d58
TomCJonesDev <196759137+TomCJonesDev@users.noreply.github.com>
2025-02-14 21:36:50 +0000

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string
+    .replace(/[^a-zA-Z0-9]/g, "")
+    .toLowerCase();
+    let array = string.split("");
+    let stringReversed = array
+    .reverse()
+    .join("");
+    return string === stringReversed;
 };
 
 // Do not edit below this line
